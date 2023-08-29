dfbc8a3d9e59d9cb324c64f0aa4fe50a6b7a22c8
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-08-29 06:56:19 +0300

    Nav bar added

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,28 +4,27 @@ const createNav = () =>{
     <div class="nav">
         <img src="img/dark-logo.png" class="brand-logo" alt="logo">
         <div class="nav-items">
-            <div class="search">
-
-
-                    <input class="search-box" type="text"
-                           placeholder="search brand,product">
-
-                <button class="search-bth">search</button>
-
-            </div>
-            <a href="#"><img src="img/user.png" alt=""></a>
-            <a href="#"><img src="img/cart.png" alt=""></a>
-
-        </div>
-
-    </div>
-    <ul class="links-container">
+        <ul class="links-container">
         <li class="link-item"><a href="#" class="link">home</a></li>
         <li class="link-item"><a href="#" class="link">women</a></li>
         <li class="link-item"><a href="#" class="link">men</a></li>
         <li class="link-item"><a href="#" class="link">kids</a></li>
         <li class="link-item"><a href="#" class="link">accessories</a></li>
 
-    </ul>`
+    </ul>
+            <div class="search">
+                    <input class="search-box" type="text"
+                           placeholder="search brand,product">
+
+                <button class="search-bth">search</button>
+
+            </div>
+            <a href="#"><img src="img/user2.png" alt=""></a>
+            <a href="#"><img src="img/cart.png" alt=""></a>
+
+        </div>
+
+    </div>
+    `
 }
 createNav();
